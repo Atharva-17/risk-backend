cb5ec30479dc7f6d5cbda85d11ca48dcc0d22278
Atharva-17 <atharvamanish50@gmail.com>
2024-03-01 16:16:09 +0530

    Initial Commit

---
+++ middlewares/auth.middleware.js
@@ -0,0 +1,36 @@
+import { asyncHandler } from "../utils/asyncHandler.js";
+import jwt from "jsonwebtoken";
+import { User } from "../models/user.model.js";
+import { ApiResponse } from "../utils/ApiResponse.js";
+
+export const verifyJWT = asyncHandler(async (req, res, next) => {
+  try {
+    const token =
+      req.cookies?.accessToken ||
+      req.header("Authorization")?.replace("Bearer ", "");
+    if (!token) {
+      return res
+        .status(401)
+        .json(new ApiResponse(401, null, "Unauthorized request1"));
+    }
+
+    const decodedToken = jwt.verify(token, process.env.ACCESS_TOKEN_SECRET);
+    const user = await User.findById(decodedToken?._id).select(
+      "-password -refreshToken"
+    );
+
+    if (!user) {
+      return res
+        .status(401)
+        .json(new ApiResponse(401, null, "Invalid access token"));
+    }
+
+    req.user = user;
+    next();
+  } catch (error) {
+    console.log("error-->", error)
+    return res
+      .status(401)
+      .json(new ApiResponse(401, null, "Unauthorized request2"));
+  }
+});
